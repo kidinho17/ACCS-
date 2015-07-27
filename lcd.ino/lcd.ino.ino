7d677c0268d8ed76b6b2d127b7521747650ec6a5
Kidinho17 <carloseuclides17@hotmail.com>
2015-07-27 22:12:49 +0200

    Implement serial capabilities to the code

--- lcd.ino/lcd.ino.ino
+++ lcd.ino/lcd.ino.ino
@@ -19,20 +19,49 @@
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+String inputString = "";         // a string to hold incoming data
+boolean stringComplete = false;  // whether the string is complete
 
 void setup() {
     Serial.begin(9600);
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   // Print a message to the LCD.
-  lcd.print("Control System");
-  lcd.setCursor(0, 1);
+  initLCD();
 }
 
 void loop() {
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
-  
+  if(stringComplete){
+    if(inputString == "clear" || inputString == "Clear" ||
+    inputString == "CLEAR" || inputString == "clr"){
+      initLCD();
+    }
+    else{
+      initLCD();
+      lcd.print(inputString);
+      inputString = "";
+      stringComplete = false;
+    }
+  }
 }
 
+
+void serialEvent() {
+  while (Serial.available()) {
+    char inChar = Serial.read(); 
+    if(inChar == ';'){
+      stringComplete = true;
+    }
+    else inputString += inChar;
+  }
+}
+
+///////////////////////////////////////////////////////////////////////
+void initLCD(){
+    lcd.clear();
+    lcd.print("Control System");
+    lcd.setCursor(0, 1);
+}
