762318e98a2a72fc90cc9a17b7631e90c9e1e0c4
Kidinho17 <carloseuclides17@hotmail.com>
2015-09-01 10:34:59 +0200

    changed from Arduino uno to Mega

--- lcd.ino/lcd.ino.ino
+++ lcd.ino/lcd.ino.ino
@@ -1,24 +1,24 @@
 /*
   The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
+ * LCD RS pin to digital pin 12 -- 40
+ * LCD Enable pin to digital pin 11 -- 38
+ * LCD D4 pin to digital pin 5 -- 41
+ * LCD D5 pin to digital pin 4 -- 39
+ * LCD D6 pin to digital pin 3 -- 37
+ * LCD D7 pin to digital pin 2 -- 35
  * LCD R/W pin to ground
  * LCD VSS pin to ground
  * LCD VCC pin to 5V
  * 10K resistor:
  * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
+ * wiper to LCD VO pin (pin 3 -- 37)
  */
 
 // include the library code:
 #include <LiquidCrystal.h>
 
 // initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(40,38,41,39,37,35);
 String inputString = "";         // a string to hold incoming data
 boolean stringComplete = false;  // whether the string is complete
 
