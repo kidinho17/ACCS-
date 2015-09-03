93f3ef4d01d3b1530ef2fe33b2f7d8a8d24a44fe
Kidinho17 <carloseuclides17@hotmail.com>
2015-09-03 20:52:01 +0200

    Added the luminosity sensor and 2 mode display that changes every 10 seconds

--- cs/cs.ino
+++ cs/cs.ino
@@ -1,4 +1,11 @@
 /*
+  Connections for TSL2561
+   ===========
+   Connect SCL to analog 
+   Connect SDA to analog 4
+   Connect VDD to 3.3V DC
+   Connect GROUND to common ground
+   
   The circuit:
  * LCD RS pin to digital pin 12 -- 40
  * LCD Enable pin to digital pin 11 -- 38
@@ -24,6 +31,11 @@
 #include <ccspi.h>
 #include <SPI.h>
 #include "utility/debug.h"
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_TSL2561_U.h>
+/*================================= TSL2561 ===========================================*/
+Adafruit_TSL2561_Unified tsl = Adafruit_TSL2561_Unified(0x39, 12345);
 
 /*================================= RT Clock  ===========================================*/
 RTC_DS1307 rtc;
@@ -62,97 +74,131 @@ boolean stringComplete = false;  // whether the string is complete
 
 unsigned long int intTime = 0, old_intTime = 0;
 int counter = 0;
-
+bool displayMode = false;
 void setup() {
    Serial.begin(9600);
-   //Setup the clock STARTS =================================================
-    #ifdef AVR
-      Wire.begin();
-    #else
-      Wire1.begin(); // Shield I2C pins connect to alt I2C bus on Arduino Due
-    #endif
-  rtc.begin();
-  if (! rtc.isrunning()) {
-    lcd.print("Clock is NOT running!");
-    // following line sets the RTC to the date & time this sketch was compiled
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-  }
+   Serial3.begin(9600);
+   Wire.begin(); //Start I2C communication protocol
+ 
+   //Setup the LCD STARTS ==============================================
+     lcd.begin(16, 2); //set up the LCD's number of columns and rows
+     lcd.clear(); // Clear the LCD
+   //Setup the LCD ENDS ===============================================
+   
+   //Setup the DTHT STARTS ============================================
+      dht.begin(); //Start DHTSensor
+   //Setup the DTHT ENDS ============================================
+    
+  //Setup the clock STARTS =================================================
+    rtc.begin();
+    if (! rtc.isrunning()) {
+      lcd.print("Clock is NOT running!");
+      // following line sets the RTC to the date & time this sketch was compiled
+      rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+      // This line sets the RTC with an explicit date & time, for example to set
+      // January 21, 2014 at 3am you would call:
+      // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+    }
   //Setup the clock ENDS =================================================
-   //set up the LCD's number of columns and rows:
-   lcd.begin(16, 2);
-  //Start DHTSensor
-    dht.begin();
-  // Clear the LCD.
-    lcd.clear();
+  
+  //Setup the TSL2561 STARTS =============================================
+    if(!tsl.begin()){
+      lcd.print("TSL2561");
+      lcd.setCursor(0,1);
+      lcd.print("not detected");
+      while(1);
+    }    
+    /* Setup the sensor gain and integration time */
+    tsl.enableAutoRange(true);            /* Auto-gain ... switches automatically between 1x and 16x */
+    tsl.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);  /* 16-bit data but slowest conversions */
+ //Setup the TSL2561 STARTS ===============================================
   
   //Setup the Wi-Fi STARTS =================================================
   lcd.print("Initializing");
   lcd.setCursor(0, 1);
   lcd.print("Wi-Fi Module");
-  if (!cc3000.begin())
-  {
-    lcd.clear(); lcd.print(F("Couldn't begin()!"));
-    while(1);
-  }
   
-  if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
-    lcd.clear(); lcd.println("Failed!");
-    while(1);
-  }
-   
-  lcd.clear(); lcd.print("Connected to:"); lcd.setCursor(0, 1); lcd.print(WLAN_SSID);
- 
-  while (!cc3000.checkDHCP()) /* Wait for DHCP to complete lcd.print("Request DHCP"); */
-  {
-    delay(100); // ToDo: Insert a DHCP timeout!
-  }  
+  //intializeWiFi();
   //Setup the Wi-Fi ENDS =================================================
 }
 
 void loop() {
-  // Read Humidity
+  //Read Humidity
   int h = dht.readHumidity();
   // Read temperature as Celsius
   int t = dht.readTemperature();
   // Check if any reads failed and exit early (to try again).
   if (isnan(h) || isnan(t)) {
-    lcd.print("Failed to read sensor!");
+    lcd.print("Failed to read");
+    lcd.setCursor(0,1);
+    lcd.print("temp. sensor!");
+    delay(5000);
     return;
   }
   else{
-    displayTemp(t,h, getTime());
+    intTime = millis();
+    int interval = intTime-old_intTime;
+    if (interval > 10000){               //10 seconds interval to display data 
+      old_intTime = intTime;
+      displayMode = !displayMode;
+    }
+    displayTemp(t,h, getTime(),readLux(), displayMode,22);
   }
   delay(1000);
-
+  /*
   //time to upload data //1 min interval
   intTime = millis();
   int interval = intTime-old_intTime;
   if (interval > 30000)    
   {
-    counter++;
+    //counter++;
     old_intTime = intTime;
+    String d = "/ACCS_Web/add.php?t=" + String("1") + "&h=" + String("1") + "&l=" + String("1") + "&fs=" + String("1") + "&date=" + getDate() + "&time="+ getTime1();
+    uploadData(d, d.length());
   }
   if(counter == 2){
     counter = 0;
     String d = "/ACCS_Web/add.php?t=" + String("1") + "&h=" + String("1") + "&l=" + String("1") + "&fs=" + String("1") + "&date=" + getDate() + "&time="+ getTime1();
     uploadData(d, d.length());
-  }
-
+  }*/
+ 
+ 
 }
-///////////////////////////////////////////////////////////////////////
-void displayTemp(int temp, int hud, String tm){
+////////////////////////////////////////////////////////////////////////////////
+float readLux(){
+  /* Get a new sensor event */ 
+  sensors_event_t event;
+  tsl.getEvent(&event);
+ 
+  /* return the results (light is measured in lux) */
+  if (event.light) {
+    return event.light; 
+  }
+  else
+  {
+    return 0.0;
+  }
+}
+void displayTemp(int temp, int hud, String tm, float l, bool mode, int dtemp){
   lcd.clear();
-  lcd.print(tm);
-  lcd.setCursor(0, 1);
-  lcd.print("T:");
-  lcd.print(temp);
-  lcd.print("*C H:");
-  lcd.print(hud);
-  lcd.print("% L:");
-  lcd.print("0");
+  if(mode){
+    lcd.print(tm);
+    lcd.setCursor(0, 1);
+    lcd.print("Target Temp:");
+    lcd.print(dtemp);
+    lcd.print((char)223);
+    lcd.print("C");
+  }
+  else{
+    lcd.print("Temp  Hum  Lux");
+    lcd.setCursor(0, 1);
+    lcd.print(temp);
+    lcd.print((char)223);
+    lcd.print("C  ");
+    lcd.print(hud);
+    lcd.print("%  ");
+    lcd.print(l);
+  }
 }
 
 String getTime() {
@@ -185,8 +231,9 @@ String getTime1() {
 /*================================= Updload data ============================*/
 void uploadData(String d, int l){
   
-   char data[100] = {};
-   d.toCharArray(data, l);  
+  //intializeWiFi(); 
+  char data[100] = {};
+  d.toCharArray(data, l);  
   
   uint32_t address = 167772164; //Server ip address 
   Adafruit_CC3000_Client www = cc3000.connectTCP(address, 80);
@@ -194,7 +241,8 @@ void uploadData(String d, int l){
     www.fastrprint(F("GET "));
     www.fastrprint(data);
     www.fastrprint(F(" HTTP/1.1\r\n"));
-    www.fastrprint(F("Host: http:10.0.0.4")); 
+    www.fastrprint("Host: ");
+    www.fastrprint(WEBSITE);
     www.fastrprint(F("\r\n"));
     www.fastrprint(F("\r\n"));
     www.println();
@@ -206,16 +254,51 @@ void uploadData(String d, int l){
     delay(10000);    
     return;
   }
+  lcd.clear();
+  lcd.print("Data uploaded"); 
+  delay(5000);
   
-  /* Read data until either the connection is closed, or the idle timeout is reached. */ 
+  
+  /* Read data until either the connection is closed, or the idle timeout is reached. */
   unsigned long lastRead = millis();
   while (www.connected() && (millis() - lastRead < IDLE_TIMEOUT_MS)) {
     while (www.available()) {
       char c = www.read();
-      //Serial.print(c);
+      Serial.print(c);
       lastRead = millis();
     }
   }
   www.close();  
   cc3000.disconnect();
+}
+  /*=============================================================*/
+void intializeWiFi(){
+  //=================================================================== 
+   if (!cc3000.begin())
+  {
+    lcd.clear(); 
+    lcd.print("Couldn't begin");
+    lcd.setCursor(0, 1);
+    lcd.print("the module");
+    while(1);
+  }
+  if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
+    lcd.clear(); 
+    lcd.println("Failed to");
+    lcd.setCursor(0, 1);
+    lcd.print("the network"); 
+    while(1);
+  }  
+  while (!cc3000.checkDHCP()) /* Wait for DHCP to complete lcd.print("Request DHCP"); */
+  {
+    delay(100); // ToDo: Insert a DHCP timeout!
+  }
+  //===============================================================
+}
+void serialEvent(){
+   while (Serial.available() > 0) {
+      // get the new byte
+      char inChar = Serial.read();
+      Serial3.print(inChar);
+  }
 }
